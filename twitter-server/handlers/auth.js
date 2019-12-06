2463cd671f9f0cf90ec2126ba805695a85b82b95
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-06 15:30:03 +0200

    add signin, start message model

--- twitter-server/handlers/auth.js
+++ twitter-server/handlers/auth.js
@@ -1,8 +1,41 @@
 const db = require('../models'),
       jwt = require('jsonwebtoken');
 
-exports.signin = function(){
-    
+exports.signin = async function(req,res,next){
+    try {
+        //finding a user
+        let user = await db.User.findOne({
+            email: req.body.email
+        });
+        //checking if password matches the typed in password
+        let {id, username, profileImageUrl} = user;
+        let isMatch = await user.comparePassword(req.body.password);
+        if(isMatch){
+            let token = jwt.sign({
+                id,
+                username,
+                profileImageUrl
+            },
+            process.env.SECRET_KEY
+            );
+            return res.status(200).json({
+                id,
+                username,
+                profileImageUrl,
+                token
+            });
+        } else {
+            return next({
+                status: 400,
+                message: 'Invalid Email or Password'
+            })
+        }
+    } catch(e){
+        return next({
+            status: 400,
+            message: 'Invalid Email or Password'
+        })
+    }
 };
 
 exports.signup = async function(req,res,next){
