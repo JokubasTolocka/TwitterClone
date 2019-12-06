2fc11440b026537166863a5cf754646282dd3c42
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-06 10:57:30 +0200

    User schema

---
+++ twitter-server/models/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true);
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/twitter', {
+    keepAlive: true,
+    useMongoClient: true
+});
+
