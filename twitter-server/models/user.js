7d925bd44bcd5e818b24e7f62b510249fb5737d4
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-06 11:05:17 +0200

    hashed password and comparing passwords

--- twitter-server/models/user.js
+++ twitter-server/models/user.js
@@ -19,7 +19,30 @@ const userSchema = new mongoose.Schema({
     profileImageUrl: {
         type: String
     }
-})
+});
+
+//before a user is saved, we are going to run a function that hashes the password
+userSchema.pre('save', async function(next){
+    try {
+        if(!this.isModified('password')){
+            return next();
+        }
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return(next);
+    } catch(err) {
+        return next(err);
+    }
+});
+
+userSchema.method.comparePassword = async function(candidatePassword, next){
+    try {
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch(err){
+        return next(err);
+    }
+}
 
 const User = mongoose.model('User', userSchema);
 
