e1efe2e75c771afa500c417ba908284a4ddccfd4
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-09 12:54:14 +0200

    start auth form

--- twitter-client/src/Containers/Main.js
+++ twitter-client/src/Containers/Main.js
@@ -2,12 +2,23 @@ import React from 'react';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Homepage from '../Components/Homepage';
+import AuthForm from '../Components/AuthForm';
 
 const Main = props => {
     return(
         <div className='container'>
             <Switch>
                 <Route exact path='/' render={props => <Homepage{...props}/>}></Route>
+                <Route exact path='/signin' render={props => {
+                    return (
+                        <AuthForm buttonText='Log in' heading='Welcome Back.' {...props}/>
+                    )
+                }}/>
+                <Route exact path='/signup' render={props => {
+                    return (
+                        <AuthForm buttonText='Sign me up!' heading='Join Twitter today.' {...props}/>
+                    )
+                }}/>
             </Switch>
         </div>
     )
