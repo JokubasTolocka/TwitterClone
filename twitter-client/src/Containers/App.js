6bb2564baaa4a153d48dc621c890c472dc1a4b7b
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-10 10:25:28 +0200

    displaying messages

--- twitter-client/src/Containers/App.js
+++ twitter-client/src/Containers/App.js
@@ -4,8 +4,20 @@ import {configureStore} from '../store';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './Navbar';
 import Main from './Main';
+import { setAuthorizationToken , setCurrentUser} from '../store/actions/auth';
+import jwtDecode from 'jwt-decode';
 
 const store = configureStore();
+
+if(localStorage.jwtToken){
+  setAuthorizationToken(localStorage.jwtToken);
+  //prevent someone manually tampering with jwt key
+  try{
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+  } catch(e){
+    store.dispatch(setCurrentUser(e));
+  }
+}
 
 const App = () => (
   <Provider store={store}>
--- twitter-client/src/store/actions/auth.js
+++ twitter-client/src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import {apiCall} from '../../services/api';
+import {apiCall , setTokenHeader} from '../../services/api';
 import {SET_CURRENT_USER} from '../actionTypes';
 import {addError, removeError} from './errors';
 
@@ -10,9 +10,14 @@ export function setCurrentUser(user){
     };
 }
 
+export function setAuthorizationToken(token) {
+    setTokenHeader(token);
+}
+
 export function logout(){
     return dispatch => {
         localStorage.clear();
+        setAuthorizationToken(false);
         dispatch(setCurrentUser({}));
     }
 }
